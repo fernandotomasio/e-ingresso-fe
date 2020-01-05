afe8339b6ea8be3085f6b84991c808e5542d6335
Fernando Tomasio <fernandotomasio@gmail.com>
2020-01-05 11:59:02 -0200

    add pessoa components

---
+++ src/app/components/pessoa-components/pessoa-detail/pessoa-detail.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {PessoaService} from '../../../core/pessoa.service';
+
+@Component({
+  selector: 'ein-pessoa-detail',
+  templateUrl: './pessoa-detail.component.html',
+  styleUrls: ['./pessoa-detail.component.scss']
+})
+export class PessoaDetailComponent implements OnInit {
+
+  @Input() oid: number;
+
+  data: any;
+
+  constructor(private service: PessoaService) { }
+
+  ngOnInit() {
+    this.service.find(this.oid).subscribe(response => {
+      this.data = response;
+    });
+  }
+
+
+}
---
+++ src/app/components/pessoa-components/pessoa-form/pessoa-form.component.ts
@@ -0,0 +1,48 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PessoaService} from '../../../core/pessoa.service';
+
+@Component({
+  selector: 'ein-pessoa-form',
+  templateUrl: './pessoa-form.component.html',
+  styleUrls: ['./pessoa-form.component.scss']
+})
+export class PessoaFormComponent implements OnInit {
+
+  @Input() oid: number;
+  form: FormGroup;
+  @Output() saved = new EventEmitter<any>();
+  @Output() canceled = new EventEmitter();
+
+  constructor(private service: PessoaService,
+              private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      oid: this.fb.control(''),
+      nome: this.fb.control('', Validators.required),
+      saram: this.fb.control(''),
+      cpf: this.fb.control('', Validators.required),
+
+    });
+
+    if (this.oid) {
+      this.service.find(this.oid)
+        .subscribe(data =>
+          this.form.patchValue(data));
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.service.save(this.form.value).subscribe(response => {
+        this.saved.emit(response);
+      });
+    }
+  }
+
+  onCancel() {
+    this.canceled.emit('canceled');
+  }
+
+}

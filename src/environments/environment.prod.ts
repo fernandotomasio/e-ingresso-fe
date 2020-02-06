e0fc00650b9e1ef8116438876e0add11b7573484
aguiardafa <diego.fernandes.aguiar@gmail.com>
2020-02-06 15:11:18 -0300

    fix endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_endpoint: 'http://servicos.ccarj.intraer/e-ingresso-fe',
+  api_endpoint: 'http://servicos.ccarj.intraer/e-ingresso-api',
   compepe_url: 'http://api.servicos.homolog.ccarj.intraer/AcessoRWS',
   app_context: ''
 };

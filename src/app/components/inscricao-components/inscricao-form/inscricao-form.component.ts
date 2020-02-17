fb513dbe424d8b548c85c7f7dee2d28dd50f5471
Fernando Tomasio <fernandotomasio@gmail.com>
2020-02-17 20:19:10 -0300

    REMOVE SELECT PESSOA FROM INSCRIÇÃO

--- src/app/components/inscricao-components/inscricao-form/inscricao-form.component.ts
+++ src/app/components/inscricao-components/inscricao-form/inscricao-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InscricaoService } from '../../../core/inscricao.service';
+import {AppService} from '../../../app.service';
 
 @Component({
   selector: 'ein-inscricao-form',
@@ -16,12 +17,13 @@ export class InscricaoFormComponent implements OnInit {
   @Input() oid: any
 
   constructor(private service: InscricaoService,
+              private appService: AppService,
               private fb: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.fb.group({
       oid: this.fb.control(''),
-      pessoaOid: this.fb.control('', Validators.required),
+      pessoaOid: this.appService.getUser().oid,
       eventoOid: this.eventoOid,
       email: this.fb.control('', [Validators.required, Validators.maxLength(255), Validators.email]),
       telefone: this.fb.control('', [Validators.required, Validators.maxLength(255)]),

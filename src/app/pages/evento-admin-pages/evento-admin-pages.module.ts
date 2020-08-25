d1fd1c124165a23e9791bb9f0700ac6103293276
jonathanjcs <jonathanjcs@fab.mil.br>
2020-08-25 13:27:52 -0300

    melhorando layout de evento

--- src/app/pages/evento-admin-pages/evento-admin-pages.module.ts
+++ src/app/pages/evento-admin-pages/evento-admin-pages.module.ts
@@ -6,6 +6,7 @@ import { EventoAdminPagesRoutes } from './evento-admin-pages-routing';
 import { EventoEditPageComponent } from './evento-edit-page/evento-edit-page.component';
 import { EventoDetailPageComponent } from './evento-detail-page/evento-detail-page.component';
 import { EventoComponentsModule } from '../../components/evento-components/evento-components.module';
+import {DemoMaterialModule} from '../../demo-material-module';
 
 
 
@@ -13,7 +14,9 @@ import { EventoComponentsModule } from '../../components/evento-components/event
   declarations: [EventoIndexPageComponent, EventoEditPageComponent, EventoDetailPageComponent],
   imports: [
     CommonModule, EventoComponentsModule,
-    RouterModule.forChild(EventoAdminPagesRoutes)
+    RouterModule.forChild(EventoAdminPagesRoutes),
+    DemoMaterialModule
+
   ]
 })
 export class EventoAdminPagesModule { }
